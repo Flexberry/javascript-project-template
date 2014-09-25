6a8fb659168ab5602ffdd22917660d17a0c82e11
nagits <aleksey.nagits@gmail.com>
2014-09-25 18:41:23 +0600

    Reindent some files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,62 @@
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-githooks');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jslint');
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/*.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-	
-	githooks: {
-	  all: {
-	    'pre-commit': 'test',
-		options: {
-			hashbang: '#!/bin/sh',
-			template: 'githook.hb',
-			startMarker: '## LET THE FUN BEGIN',
-			endMarker: '## PARTY IS OVER',
-			preventExit: false
-		}
-	  }
-	},
-	
-	jshint: {
-      files: ['src/*.js'],
-      options: {
-        jshintrc: true
-      }
-    },
-	
-	jslint: {
-	  all: {
-	    src: ['src/*.js'],
-        options: {
-          errorsOnly: false,
-          failOnError: true
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/*.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
         },
-	    directives: {
-		  white: false,
-          devel: true
-	    }
-	  }
-	}
-  });
 
-  // Default task(s).
-  // githooks - Binds grunt tasks to git hooks
-  grunt.registerTask('default', ['githooks', 'uglify']);
+        githooks: {
+            all: {
+                'pre-commit': 'test',
+                options: {
+                    hashbang: '#!/bin/sh',
+                    template: 'githook.hb',
+                    startMarker: '## LET THE FUN BEGIN',
+                    endMarker: '## PARTY IS OVER',
+                    preventExit: false
+                }
+            }
+        },
 
-  grunt.registerTask('test', ['jshint', 'jslint']);
-  
-  grunt.registerTask('travis', ['jshint', 'jslint']);
+        jshint: {
+            files: ['src/*.js'],
+            options: {
+                jshintrc: true
+            }
+        },
+
+        jslint: {
+            all: {
+                src: ['src/*.js'],
+                options: {
+                    errorsOnly: false,
+                    failOnError: true
+                },
+                directives: {
+                    white: false,
+                    devel: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-githooks');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jslint');
+
+    // githooks - Binds grunt tasks to git hooks
+    grunt.registerTask('default', ['githooks', 'uglify']);
+
+    grunt.registerTask('test', ['jshint', 'jslint']);
+
+    grunt.registerTask('travis', ['jshint', 'jslint']);
 };
