e3c091963fc60ae9ec4225d8d62c5c21beeff444
Flexberry <mail@flexberry.net>
2014-10-02 06:17:01 +0000

    auto deploy

---
+++ build/Flexberry.js
@@ -0,0 +1,17 @@
+/*! Flexberry - v1.0.0 - 2014-10-02 */
+(function () {
+    'use strict';
+
+    function getStr() {
+        var str = 'Hello World!!!';
+        return str;
+    }
+
+    console.log(getStr());
+    console.log('test');
+
+    var mybool = false;
+    if (mybool) {
+        console.log('jshint test');
+    }
+}());
