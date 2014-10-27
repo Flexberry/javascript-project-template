380a0f5a29a0fa1d762ea85c717f6db9e3faeccc
nagits <aleksey.nagits@gmail.com>
2014-10-27 12:43:21 +0600

    auto publish

---
+++ build/Flexberry-javascript-project-template.js
@@ -0,0 +1,17 @@
+/*! Flexberry-javascript-project-template - v1.0.0 - 2014-10-27 */
+(function() {
+    'use strict';
+
+    function getStr() {
+        var str = 'Hello World!!!';
+        return str;
+    }
+
+    console.log(getStr());
+    console.log('test');
+
+    var mybool = false;
+    if (mybool) {
+        console.log('jshint test');
+    }
+}());
