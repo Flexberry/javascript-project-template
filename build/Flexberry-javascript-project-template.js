ebcdad96da40dc63527dc7c1b1d7d801e3c73513
Flexberry <mail@flexberry.net>
2014-10-31 11:53:43 +0000

    auto deploy Reason: d2263a9d632f974a83fefd2b552cf367e16f4cdd

--- build/Flexberry-javascript-project-template.js
+++ build/Flexberry-javascript-project-template.js
@@ -1,4 +1,4 @@
-/*! Flexberry-javascript-project-template - v1.0.0 - 2014-10-30 */
+/*! Flexberry-javascript-project-template - v1.0.0 - 2014-10-31 */
 // Uses Node, AMD or browser globals to create a module.
 (function(root, factory) {
     'use strict';
